283dfb75114abe212d6c7230e8812a3122d3fe65
jsososo <jsososo@outlook.com>
2017-06-19 22:45:18 +0800

    init project

---
+++ src/components/Input.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import InputBox from './InputBox';
+
+export default class Input extends React.Component {
+  render () {
+    return (
+      <div className="input-container">
+        <InputBox/>
+      </div>
+    )
+  }
+}
---
+++ src/components/InputBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class InputBox extends React.Component {
+  render () {
+    return (
+      <div className="input-box">
+        <input type="text"/>
+      </div>
+    )
+  }
+}
