c447f2540cb3de4c8c94906ea4c57c73d7837705
jsososo <jsososo@outlook.com>
2017-06-19 23:08:06 +0800

    init project

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -29,20 +29,8 @@ function getDefaultModules() {
         loader: 'style-loader!css-loader'
       },
       {
-        test: /\.sass/,
-        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded&indentedSyntax'
-      },
-      {
         test: /\.scss/,
-        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
-      },
-      {
-        test: /\.less/,
-        loader: 'style-loader!css-loader!less-loader'
-      },
-      {
-        test: /\.styl/,
-        loader: 'style-loader!css-loader!stylus-loader'
+        loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader?outputStyle=expanded'
       },
       {
         test: /\.(png|jpg|gif|woff|woff2)$/,
